cb14556516e7804ac93e6d170ae833242d9bf64a
DipankarDas <dipsonu10@hotmail.com>
2022-01-05 23:19:41 +0530

    PATCH 5/1/22 08-Strings {MEDIUM} Q4 DONE

--- assignments/08-strings/src/Medium/Q3.java
+++ assignments/08-strings/src/Medium/Q3.java
@@ -7,23 +7,23 @@ package Medium;
 public class Q3 {
     public int numWays(String s) {
     	int ones = 0;
-	final int m = 1_000_000_007;
-	int len = s.length();
+		final int m = 1_000_000_007;
+		int len = s.length();
 
-	for (int i = 0; i<len; i++)
-		ones += s.charAt(i) - '0';
+		for (int i = 0; i<len; i++)
+			ones += s.charAt(i) - '0';
 
-	if(ones == 0)
-		return (int)((len - 2L)*(len - 1L)/2%m);
+		if(ones == 0)
+			return (int)((len - 2L)*(len - 1L)/2%m);
 
-	if(ones%3 != 0)
-		return 0;
+		if(ones%3 != 0)
+			return 0;
 
-	int noOfOnesInBlock = ones / 3;
-	long first=0, second = 0;
+		int noOfOnesInBlock = ones / 3;
+		long first=0, second = 0;
 
-	for (int i = 0, count=0; i<len; i++) {
-		count += s.charAt(i) - '0';
+		for (int i = 0, count=0; i<len; i++) {
+			count += s.charAt(i) - '0';
 
 		if(count == noOfOnesInBlock)
 			first++;
@@ -34,8 +34,8 @@ public class Q3 {
 	return (int)(first*second%m);
     }
     public static void main(String[] args) {
-	String s="10101";
-	Q3 ob = new Q3();
-	System.out.println(ob.numWays(s));
-    }
+		String s="10101";
+		Q3 ob = new Q3();
+		System.out.println(ob.numWays(s));
+	}
 }
--- assignments/08-strings/src/Medium/Q4.java
+++ assignments/08-strings/src/Medium/Q4.java
@@ -2,10 +2,36 @@ package Medium;
 
 /**
  * Q4
+ * Split the two sentences into words;
+ * Scan the shorter one from left and compare word by word with the longer; Whenever the words from the 2 sentences are not equal, compare the tails of the 2 sentences word by word till the end or they do not match;
+ * Confirm if the pointer reaches the end of shorter sentence. If yes, return true; otherwise, return false.
  */
 public class Q4 {
 
-    public static void main(String[] args) {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        int i=0;
+        sentence1+=" ";
+        sentence2+=" ";
+        String[] w1=sentence1.split(" ");
+        String[] w2=sentence2.split(" ");
+        int len1=w1.length, len2=w2.length;
 
+        if(len1 > len2) {
+            return areSentencesSimilar(sentence2,sentence1);
+        }
+
+        while (i<len1 && w1[i].equals(w2[i])) {
+            i++;
+        }
+
+        while (i<len1 && w1[i].equals(w2[len2-len1+i])) {
+            i++;
+        }
+        return i == len1;
+    }
+
+    public static void main(String[] args) {
+        Q4 obj = new Q4();
+        System.out.println(obj.areSentencesSimilar("of", "A lot of words"));
     }
 }
