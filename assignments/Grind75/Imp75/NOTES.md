c235d750be8111779d7603b6160e876208636762
Dipankar Das <dipankardas0115@gmail.com>
2022-08-25 20:20:14 +0530

    2/75 Done 75Grind

--- assignments/Grind75/Imp75/NOTES.md
+++ assignments/Grind75/Imp75/NOTES.md
@@ -8,4 +8,4 @@ Grind 75
 ---
 
 1. TwoSum (**EASY**) Used HasMap for storing the current <element,index> when target-this_no not found
-2. 
+2. ValidPranthesis (**EASY**) USed Stack to keep track of the brackets visited and used balance variable
---
+++ assignments/Grind75/Imp75/src/ValidParanthesis.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> exp = new Stack<>();
+        int balance = 0;
+
+        for (Character bracket : s.toCharArray()) {
+            if (bracket == '(' || bracket == '[' || bracket == '{') {
+                exp.push(bracket);
+                balance--;
+            } else {
+                balance++;
+                while (!exp.isEmpty() && !gotIt(bracket, exp.peek())) {
+                    exp.pop();
+                }
+
+                if (exp.isEmpty())
+                    return false;
+                else
+                    exp.pop();
+            }
+        }
+        return balance == 0;
+    }
+
+    private boolean gotIt(Character exp, Character peek) {
+        return switch (exp) {
+            case ')' -> peek == '(';
+            case '}' -> peek == '{';
+            case ']' -> peek == '[';
+            default -> false;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidParanthesis().isValid("()[]{}"));
+    }
+}
