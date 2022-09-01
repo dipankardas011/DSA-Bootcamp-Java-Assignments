f67df5c07d77d3fcf741920e7134f0d6e41a7325
Dipankar Das <dipankardas0115@gmail.com>
2022-09-01 22:34:32 +0530

    5/75 Grid75 DONE

--- assignments/Grind75/Imp75/NOTES.md
+++ assignments/Grind75/Imp75/NOTES.md
@@ -12,3 +12,4 @@ Grind 75
 3. merge2SortedLists (**EASY**) as it's given in sorted list, and we have to merge it; use the merge procedure
 4. BestTimetoBuyandSellStock (**EASY**) as loop inside loop will have N^2 so to get max from the remaning elements in right in O(1)
             we store the max elements from right to left in another array when the element is and maxEle from that index in maxEleme Array is the answer
+5. ValidPalindrome (**EASY**) filter the noise from the string and then check palindrome using left and right pointers
---
+++ assignments/Grind75/Imp75/src/ValidPalindrome.java
@@ -0,0 +1,30 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        // only compare letters and numbers and convert uppercase eot lowercase
+        s = s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetterOrDigit(ch))
+                str.append(ch);
+        }
+        // System.out.println(str);
+        // return false;
+        s = str.toString();
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(new ValidPalindrome().isPalindrome("0P"));
+    }
+}
