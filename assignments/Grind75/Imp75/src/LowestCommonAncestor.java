0cb6986b98b449510f502c2ca2cd9bc99a1521e4
Dipankar Das <dipankardas0115@gmail.com>
2022-09-30 11:24:18 +0530

    10/75 Done Grid75
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

---
+++ assignments/Grind75/Imp75/src/LowestCommonAncestor.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else
+            return root; 
+    }
+
+    public static void main(String[] args) {
+        // driver
+    }
+}
