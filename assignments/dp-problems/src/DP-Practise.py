ed8654091d71f44095412e2f53e2d4d3654603f6
Dipankar Das <dipankardas0115@gmail.com>
2022-10-29 12:59:04 +0530

    Added the Dynamic programming practise - [x] LongestCommonSubSequence - [x] TotalNoOfSubSetGivesTotal
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- assignments/dp-problems/src/DP-Practise.py
+++ assignments/dp-problems/src/DP-Practise.py
@@ -20,7 +20,6 @@ def MaxSequenceDP(arr: list[int], total: int, index: int, mem: list[list[int]])-
     return res
 
 
-
 def MaxSequence(arr: list[int], total: int, index: int)-> int:
     if total < 0:
         return 0
@@ -35,6 +34,39 @@ def MaxSequence(arr: list[int], total: int, index: int)-> int:
         MaxSequence(arr, total-arr[index], index-1)
     )
 
+
+def LongestCommonSequence(p: str, q: str, m: int, n: int) -> int:
+    if m == 0 or n == 0:
+        return 0
+    if p[m] == q[n]:
+        res = 1 + LongestCommonSequence(p,q,m-1,n-1)
+    else:
+        res = max(
+            LongestCommonSequence(p, q, m, n-1),
+            LongestCommonSequence(p, q, m-1, n)
+        )
+    return res
+
+
+def LongestCommonSequenceDP(p: str, q: str, m: int, n: int, mem: list[list[int]]) -> int:
+    if mem[m][n] != -1:
+        return mem[m][n]
+
+    if m == 0 or n == 0:
+        res = 0
+    elif p[m] == q[n]:
+        res = 1 + LongestCommonSequenceDP(p,q,m-1,n-1, mem)
+    else:
+        res = max(
+            LongestCommonSequenceDP(p, q, m, n-1, mem),
+            LongestCommonSequenceDP(p, q, m-1, n, mem)
+        )
+    mem[m][n] = res
+    return res
+
+
+
+
 # find all number of subsequence with same total
 arr = [1,3,7,5,2]
 total = 10
@@ -43,3 +75,18 @@ memory: list[list[int]] = [ [-1]*(total+1) ]*(len(arr))
 
 print("No of Subsets using RT: %d"%MaxSequence(arr, total, len(arr)-1))
 print("No of Subsets using DP: %d"%MaxSequenceDP(arr, total, len(arr)-1, memory))
+
+
+str1 = "abb"
+str2 = "bcb"
+print(f"No of longest Common substrings b/w {str1} {str2} = {LongestCommonSequence(str1, str2, len(str1)-1, len(str2)-1)}")
+mem: list[list[int]] = [ [-1] * (len(str2)) ] * (len(str1))
+print(f"No of longest Common substrings b/w {str1} {str2} = {LongestCommonSequenceDP(str1, str2, len(str1)-1, len(str2)-1, mem)}")
+
+str1 = "aaa"
+str2 = "bbb"
+print(f"No of longest Common substrings b/w {str1} {str2} = {LongestCommonSequence(str1, str2, len(str1)-1, len(str2)-1)}")
+mem = [ [-1] * (len(str2)) ] * (len(str1))
+print(f"No of longest Common substrings b/w {str1} {str2} = {LongestCommonSequenceDP(str1, str2, len(str1)-1, len(str2)-1, mem)}")
+
+
